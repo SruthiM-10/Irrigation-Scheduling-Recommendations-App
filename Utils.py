b21650a8d432df6891896e3411aa62c3d74feaaa
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2025-12-15 19:03:40 -0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 
-def load_pickle_files(model_path: str) --> object:
+def load_pickle_files(model_path: str): --> object
   with open(model_path, "rb") as f:
     model = pickle.load(f)
   return model
