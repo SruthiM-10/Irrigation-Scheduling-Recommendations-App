be7b79e3e0d6ea6f6c9d52b5b49791686f43f33a
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2026-03-02 03:12:45 -0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,7 +1,7 @@
-import pickle
+import joblib
 import streamlit as st
 
-def load_pickle_files(model_path: str):
-  with open(model_path, "rb") as f:
-    model = pickle.load(f)
+def load_model(model_type: str):
+  model_path = f"Models/{model_type}.pkl"
+  model = joblib.load(model_path)
   return model
