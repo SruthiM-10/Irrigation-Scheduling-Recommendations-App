cc67141cf030943f8a34638a4a85158f7e72321e
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2026-03-02 22:12:58 -0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -7,11 +7,9 @@ def load_model(model_type: str):
   model = joblib.load(model_path)
   return model
 
-def load_related_data(user_data):
+def load_related_data(user_data, method):
   dataset = pd.read_csv("final_dataset_updated_doi.csv")
-  dataset = dataset[dataset['irrigation_scheduling_method_standardized'] == user_data.loc[0, 'irrigation_scheduling_method_standardized']]
-  print(user_data.loc[0, 'irrigation_scheduling_method_standardized'])
-  print("Current Length of Dataset", len(dataset))
+  dataset = dataset[dataset['irrigation_scheduling_method_standardized'] == method]
 
   dataset['distance'] = (dataset['latitude_decimal_degrees'] - user_data.loc[0, 'latitude_decimal_degrees'])**2 + \
                         (dataset['longitude_decimal_degrees'] - user_data.loc[0, 'longitude_decimal_degrees'])**2
