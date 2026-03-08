9a94f77a9d99852a7fa0d8ccabc4875171b03828
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2026-03-08 12:02:49 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -35,7 +35,7 @@ st.markdown(
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: rgba(255, 255, 255, 0.6); /* Adjust opacity here */
+        background-color: rgba(255, 255, 255, 0.1); /* Adjust opacity here */
         z-index: -1;
      }}
      </style>
