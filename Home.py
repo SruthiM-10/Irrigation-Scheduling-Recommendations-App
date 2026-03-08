b1d4989d2ee9665d8c8a0bc4ccb5f19971c618c9
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2026-03-08 12:45:06 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -133,9 +133,9 @@ if st.session_state.page == 'home':
         col4, col5 = st.columns(2)
         with col4:
             st.markdown("""
-                <div style="display: flex; align-items: center; margin-bottom: 2px;">
+                <div style="display: flex; align-items: center; margin-bottom: 5px;">
                     <span style="font-size: 14px; margin-right: 5px">Soil Type</span>
-                    <a href="https://www.nrcs.usda.gov/sites/default/files/2022-09/The-Soil-Texture-Triangle.pdf" 
+                    <a href="https://www.nano-yield.com/the-soil-texture-triangle" 
                        target="_blank" 
                        title="View USDA Soil Texture Chart" 
                        style="text-decoration: none; color: #2e8b57; cursor: pointer">
