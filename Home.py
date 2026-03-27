8bbbf6354500543b9c139a89cbcf11c095c30540
SruthM-10 <manojsruthi10@gmail.com>
2026-03-27 10:17:15 -0700

    Fixed small syntax bug

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Irrigation Scheduling Method Recommendation System - Home",
-    page_icon="🌾",v
+    page_icon="🌾",
     layout="wide",
     initial_sidebar_state="expanded"
 )
